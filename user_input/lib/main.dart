5c7bc943fce48d8a5674c9e813eb503ab68e39ff
Ifeanyi222 <linfitechitsolcom@gmail.com>
2024-06-02 11:04:48 +0100

    user input

---
+++ user_input/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'pages/login.dart';
+import 'pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Simple App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginPage()),
+      );
+    });
+
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+            'assets/logo.png'), // Ensure logo.png is in the assets folder
+      ),
+    );
+  }
+}
